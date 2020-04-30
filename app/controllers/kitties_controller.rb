d0381c71d8eaa2aa85a9a961106bd53b85582d85
Rey Abolofia <rabolofia@newrelic.com>
2020-04-29 22:09:18 -0700

    Ability to create new kitties.

--- app/controllers/kitties_controller.rb
+++ app/controllers/kitties_controller.rb
@@ -2,4 +2,17 @@ class KittiesController < ApplicationController
   def index
     @kitties = Kitty.all
   end
+
+  def new
+  end
+
+  def create
+    @kitty = Kitty.new(
+      name: params[:kitty][:name],
+      color: params[:kitty][:color],
+      age: params[:kitty][:age].to_i,
+    )
+    @kitty.save
+    redirect_to action: "index"
+  end
 end
